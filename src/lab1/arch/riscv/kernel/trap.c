f88f2e9557a2eac15c140f032d497dc80c100f82
bowling233 <zhubaolin228@gmail.com>
2024-09-17 01:06:46 +0800

    lab1: second half init

---
+++ src/lab1/arch/riscv/kernel/trap.c
@@ -0,0 +1,12 @@
+#include "stdint.h"
+
+void trap_handler(uint64_t scause, uint64_t sepc)
+{
+    // 通过 `scause` 判断 trap 类型
+    // 如果是 interrupt 判断是否是 timer interrupt
+    // 如果是 timer interrupt 则打印输出相关信息，并通过 `clock_set_next_event()` 设置下一次时钟中断
+    // `clock_set_next_event()` 见 4.3.4 节
+    // 其他 interrupt / exception 可以直接忽略，推荐打印出来供以后调试
+
+#error Unimplemented
+}
