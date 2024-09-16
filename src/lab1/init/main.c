2bba8210818faa084eefcb44352f4a0cd3088f36
bowling233 <zhubaolin228@gmail.com>
2024-09-17 00:16:21 +0800

    lab1: question3

--- src/lab1/init/main.c
+++ src/lab1/init/main.c
@@ -1,10 +1,25 @@
 #include "printk.h"
+#include "sbi.h"
+#include "defs.h"
 
 extern void test();
+
+#define csr_print(csr)                      \
+({                                          \
+    printk(#csr ":\t");                      \
+    long __v = csr_read(csr);               \
+    printk("%lx\n", __v);                    \
+})
 
 int start_kernel() {
     printk("2024");
     printk(" ZJU Operating System\n");
+
+    csr_print(sstatus);
+    csr_print(sie);
+    csr_print(stvec);
+    csr_print(scause);
+    csr_print(sepc);
 
     test();
     return 0;
